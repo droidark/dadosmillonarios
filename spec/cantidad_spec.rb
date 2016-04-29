111d9e001cf620d4899e4fe123ad1423adf9428b
Julio <droidark@outlook.com>
2016-04-29 11:47:51 -0500

    correcion prueba tdd ingresar cantidad

--- spec/cantidad_spec.rb
+++ spec/cantidad_spec.rb
@@ -1,10 +1,10 @@
-require '.lib/cantidad'
+require './lib/cantidad'
 
 describe Cantidad do
   it "Ingreso 5000 como cantidad a apostar" do
     cantidad = Cantidad.new
-    cantidad.validaCantidad 5000
-    cantidad.should == "Bienvenido"
+    resultado = cantidad.validaCantidad 5000
+    resultado.should == "Bienvenido"
   end
 
 end
